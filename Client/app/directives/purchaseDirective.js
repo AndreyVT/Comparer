4c70e588b111f14bb89ab97157f58042cbbab8a7
AndreyVT <TupeykoAV@gmail.com>
2015-03-12 21:59:46 +0700

    night commit

---
+++ Client/app/directives/purchaseDirective.js
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: AVT
+ * Date: 12.03.15
+ * Time: 21:06
+ * To change this template use File | Settings | File Templates.
+ */
+
+mainModule.directive('cmprPurchase', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            purchaseInfo: '=purchase'
+        },
+        templateUrl: 'app/directives/purchaseTemplate.html'
+    };
+})
